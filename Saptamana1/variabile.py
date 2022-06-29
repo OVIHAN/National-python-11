020e0dfb9757b0676afaaa8fd46b3eb949787ab3
unknown <ovidiu.handoca@gmail.com>
2022-06-29 20:22:06 +0300

    primul nostru commit

---
+++ Saptamana1/variabile.py
@@ -0,0 +1,23 @@
+""" Dezvoltarea unui program care sa calculeze venitul saptamanal al unui angajat """
+""" Venitul asptamanal = venitul/ora * nr ore """
+''' Venit saptamanal = 35 lei * 48 ore '''
+
+# venit_saptamanal ----> float
+# venit_pe_ora ----> float
+# nr_ore ----> float
+
+''' 1. Obtine venitul ora '''
+''' 2. Obtine nr de ore lucrate '''
+''' 3. Calcul venit saptamanal '''
+''' 4. Afisare rezultate '''
+
+venit_pe_ora = 35.5
+nr_ore = 40
+venit_saptamanal = venit_pe_ora * nr_ore
+print("venitul saptamanal este de " + str(venit_saptamanal) + "lei")
+
+first_number = 1
+first_number = first_number + 4
+print(first_number)
+
+
