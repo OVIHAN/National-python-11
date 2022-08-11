25fea3570513181d01a111b201b2308410693b7b
Ovidiu HANDOCA <ovidiu.handoca@savencia.com>
2022-08-11 16:15:54 +0300

    mesaj

---
+++ exercitii_examen/Operatori.py
@@ -0,0 +1,60 @@
+#           OPERATORI ARITMETICI        #
+
+#   +  --> adunare --> adunarea matematica a doua sau mai multe valori
+#   -  --> scadere --> scaderea matematica a doua sau mai multe numere
+#   *  --> inmultire --> inmultirea matematica a doua sau mai multe numere
+#   /  --> impartire --> impartirea matematica a doua sau mai multe numere
+#   %  --> modulo --> restul impartirii unei valori la o alta valoare
+#   **  --> ridicare la putere --> ridicarea unei valori la o anumita valoare
+#   //  --> impartire exacta --> catul impartirii a doua valori
+
+
+#           OPERATORI DE COMPARARE      #
+
+#   ==  --> egalitate
+#   !=  --> inegalitate
+#   >   --> mai mare
+#   <   --> mai mic
+#   >=  --> mai mare sau egal
+#   <=  --> mai mic sau egal
+
+
+#           OPERATORI LOGICI      #
+
+#   and  --> si logic
+#   or   --> sau logic
+#   not  --> negare
+
+
+#           OPERATORI DE IDENTITATE      #
+
+#   is  --> este evaluata ca True daca ambele valori reprezinta aceeasi locatie de memorie
+#   is not  --> este evaluata True daca cele doua valori reprezinta locatii de memorie diferite
+
+
+#           OPERATORI DE APARTENENTA      #
+
+#   in  --> este evaluata ca true daca secventa evaluata face parte din obiect
+#   not in  --> este evaluata ca True daca secventa evaluata nu face parte din obiect
+
+
+#           OPERATORI PE BITI      #
+
+#   &  --> AND
+#   |  --> OR
+#   ^  --> XOR
+#   ~  --> NOT
+#   <<  --> left shift --> adauga biti cu valoarea 0 la finalul secventei
+#   >>  --> right shift --> elimina bitii de la finalul secventei
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ exercitii_examen/Variabile-Asignare.py
@@ -0,0 +1,9 @@
+#   Asignare simpla
+my_var = 5
+
+#   Asignare prin adaugare: my_var va avea valoarea 8 (5 + 3)
+my_var += 3 # echivalent cu my_var = my_var + 3
+
+#   Asignare prin inmultire: my_var va avea valoarea 16 (8 * 2)
+my_var *= 2 # echivalent cu my_var = my_var * 2
+
